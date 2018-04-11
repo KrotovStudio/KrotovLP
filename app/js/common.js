d3f6c3c6816dca11547260af938be792a2f86890
PC_user\user_PC <maxon.pro2015@gmail.com>
2018-04-11 23:20:32 +0300

    закончена верстка десктопа кроме таблицы и попапов

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,35 @@
-/**
- * Created by User on 15.03.2018.
- */
+$( document ).ready(function() {
+
+//    slick slider
+    $('#portfolio-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: true
+    });
+
+    $('#exerienced-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: true
+    });
+
+    $('#story-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true
+    });
+
+    $('#testimonials-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true
+    });
+
+});
